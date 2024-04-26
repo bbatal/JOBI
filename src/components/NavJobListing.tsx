d9091438a946bde63819377b55f6ff56ed0a7184
bart batalinski <bartbatalinski@gmail.com>
2024-04-26 11:32:42 -0400

    (feature): mobile menu functional

--- src/components/NavJobListing.tsx
+++ src/components/NavJobListing.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 import styled from 'styled-components';
+import { device } from '../styles/breakpoints';
 
 // Assets
 import logo from '../assets/logo-elipsees.png';
 import category from '../assets/category.svg';
 
+// Dependencies
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export default function NavJobListing() {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleMobileMenu = () => {
+        setIsActive(!isActive);
+    }
+
   return (
     <Nav>
     <List>
@@ -22,31 +35,35 @@ export default function NavJobListing() {
                 <span style={{marginLeft:"10px"}}>Categories</span>
             </CategoriesExtended>
         </Categories>
-        <Item>
+        <Item className={isActive ? "active" : ""}>
             <a href="/">Home</a>
         </Item>
-        <Item>
+        <Item className={isActive ? "active" : ""}>
             <a href="/">Job</a>
         </Item>
-        <Item>
+        <Item className={isActive ? "active" : ""}>
             <a href="/">Explore</a>
         </Item>
-        <Item>
+        <Item className={isActive ? "active" : ""}>
             <a href="/">Contact</a>
         </Item>
-        <Item>
+        <Item className={isActive ? "active" : ""}>
             <a href="/">Pages</a>
         </Item>
-        <Login>
+        <Login className={isActive ? "active" : ""}>
             <div>
             <a href="/">Post Job</a>
             <Button>Login</Button>
             </div>
         </Login>
-        <Item>
+        <Item className={isActive ? "active" : ""}>
             <HireButtonExtended>Hire Top Talents</HireButtonExtended>
         </Item>
-        <Item className="toggle"><a href="/"><i className="fas fa-bars"></i></a></Item>
+        <Toggle onClick={() => handleMobileMenu()}>
+            <Button>
+                <FontAwesomeIcon icon={ faBars } />
+            </Button>
+        </Toggle>
     </List>
     </Nav>
   )
@@ -63,6 +80,18 @@ const List = styled.ul`
     align-items: center;
     color: #fff;
     max-width: 1492px;
+
+    @media ${device.sm} {
+        flex-wrap: wrap;
+        position: absolute;
+        top: 2em;
+        right: 2em;
+        left: 2em;
+
+        li a {
+            display: block;
+        }
+    }
 `
 
 const Text = styled.span`
@@ -111,13 +140,60 @@ const Login = styled.li`
         bottom: 0;
         background-color: var(--tertiary-lightGreen);
     }
+
+    @media ${device.sm} {
+        display: none;
+        width: 100%;
+        order: 2;
+        text-align: center;
+
+        button:after {
+        height: 1px;
+        width: 70%;
+        left: 50%;
+        top: 0px;
+        bottom: 5px;
+    }
+
+        &.active {
+            display: block;
+        }
+    }
 `
 
 const Categories = styled.li`
     flex-grow: 1;
+
+    @media ${device.sm} {
+        display: none;
+    }
 `
 
 const Item = styled.li`
     flex-grow: 0;
     margin-left: 2em;
+
+    @media ${device.sm} {
+        order: 3;
+        width: 100%;
+        text-align: center;
+        display: none;
+
+        &.active {
+            display: block;
+        }
+    }
+`
+
+const Toggle = styled(Item)`
+    display: none;
+    cursor: pointer;
+    z-index: 10;
+    font-size: 20px;
+
+    @media ${device.sm} {
+        display: block;
+        width: min-content;
+        order: 1;
+    }
 `
