e1ca2461956c83d4c23d3e66d8bb361cc8d5c106
bart batalinski <bartbatalinski@gmail.com>
2024-05-01 21:54:27 -0400

    (responsiveness): refactoring search bar to work on mobile and tablet screens as expected

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,10 @@
 // styles
 import styled from 'styled-components';
+import { device } from '../styles/breakpoints';
+
+// Dependencies
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Search() {
   return (
@@ -7,7 +12,12 @@ function Search() {
         <OuterBox>
             <InnerBox>
                 <H3>Your job title, keyword or company?</H3>
+                <div>
+                <span aria-hidden="true">
+                    <FontAwesomeIcon style={{color:"var(--secondary-gray)",marginRight: "5px"}} icon={ faSearch } />
+                </span>
                 <Input type="text" placeholder="Google"></Input>
+                </div>
             </InnerBox>
         </OuterBox>
         <OuterBox>
@@ -28,9 +38,7 @@ function Search() {
             </Select>
             </InnerBox>
         </OuterBox>
-        <>
             <Button>Search</Button>
-        </>
     </BigBox>
   )
 }
@@ -39,11 +47,16 @@ export default Search
 
 const BigBox = styled.div`
     display: flex;
+    justify-content: space-between;
     border-radius: 20px;
     background-color: white;
     position: relative;
     z-index: 15;
     margin: 2rem 0;
+
+    @media ${device.mdl} {
+        width: calc(100% - 2rem);
+    }
 `
 
 const InnerBox = styled.div`
@@ -56,11 +69,65 @@ const InnerBox = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
+
+    @media ${device.sm} {
+        padding: 0rem;
+
+        h3 {
+            display: none;
+        }
+
+        input {
+        position: relative;
+        padding: 0.4rem;
+        padding-left: 2rem;
+        border-radius: 50px;
+        border: none;
+        width: 100%;
+        }
+
+        input::placeholder {
+            color: transparent;
+        }
+
+        ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+        color: transparent;
+        }
+        ::-moz-placeholder { /* Firefox 19+ */
+        color: transparent;
+        }
+        :-ms-input-placeholder { /* IE 10+ */
+        color: transparent;
+        }
+        :-moz-placeholder { /* Firefox 18- */
+        color: transparent;
+        }
+
+        input {
+            background-image:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='100%'><text x='0' y='15' fill='gray' font-size='15'>Your job title, keyword or company?</text></svg>");
+            background-repeat: no-repeat;
+        }
+
+        input:focus {
+        background-image: none;
+        }
+    }
 `
 
 const OuterBox = styled.div`
     padding: 1rem 0 1rem 2rem;
     border: transparent;
+    flex-grow: 1;
+
+    @media ${device.sm} {
+        padding: 0rem;
+    }
+
+    :nth-child(2) {
+        @media ${device.md} {
+            display: none;
+        }
+    }
 `
 
 const Input = styled.input`
@@ -102,4 +169,9 @@ const Button = styled.button`
     letter-spacing: 0.1px;
     font-weight: 200;
     cursor: pointer;
+    flex-grow: 0;
+
+    @media ${device.sm} {
+        display: none;
+    }
 `
